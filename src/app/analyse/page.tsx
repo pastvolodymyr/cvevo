4e2b572cf1dcbbe13be8d5353c5944ac5a9827e6
Volodymyr Pastukh <pastvolodymyr@gmail.com>
2024-06-26 13:52:08 +0300

    Add analyse page

--- src/app/analyse/page.tsx
+++ src/app/analyse/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Loader } from '@/components/UI';
+
 import styles from './style.module.scss';
 
 export default function Analyse() {
@@ -9,7 +11,26 @@ export default function Analyse() {
                 <div className={ styles.heroBlock }>
                     <h1>CV analyser</h1>
                     <p>
-                      Follow our easy-to-implement suggestions to improve your CV
+                        Follow our easy-to-implement suggestions to improve your CV
+                    </p>
+                </div>
+                <div className={ styles.stepBlock }>
+                    <h2>Step One</h2>
+                    <p>
+                        Upload your CV in PDF or PNG/JPEG format
+                    </p>
+                    <Loader />
+                </div>
+                <div className={ styles.stepBlock }>
+                    <h2>Step Two</h2>
+                    <p>
+                        Follow our easy-to-implement suggestions to improve your CV
+                    </p>
+                </div>
+                <div className={ styles.stepBlock }>
+                    <h2>Finish</h2>
+                    <p>
+                        Submit your polished CV with confidence and get noticed by top employers
                     </p>
                 </div>
             </div>
---
+++ src/components/UI/Loader/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './style.module.scss';
+
+export const Loader = () => {
+    return (
+        <div className={ styles.loader }/>
+    );
+};
