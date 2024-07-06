5ac8c0d2f34a631a10ea273544868e1e1cbc4fe5
Volodymyr Pastukh <pastvolodymyr@gmail.com>
2024-07-06 16:28:54 +0300

    Refactor

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import cx from 'classnames';
 
 import styles from './style.module.scss';
-import cx from 'classnames';
 
 export const Button = ({ text, onClick, disabled }: { text: string, onClick?: () => void, disabled?: boolean }) => {
     return <button className={ cx(styles.button, { [styles.buttonDisabled]: disabled }) } onClick={ !disabled ? onClick : () => {} }>{text}</button>;
